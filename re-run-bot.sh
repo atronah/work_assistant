6e81163fb7421f4fd4bde5713d6b93a69246897f
atronah <atronah@users.noreply.github.com>
2022-08-20 20:50:24 +0300

    meta(re-run-bot): switch to using systemd service
    instead direct call `python bot.py`

--- re-run-bot.sh
+++ re-run-bot.sh
@@ -1,19 +1,8 @@
 #! /usr/bin/env bash
 
-script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-pkill -f "python3 ${script_path}/bot.py"
-
-pushd "${script_path}"
-
-if [ ! -d "venv" ]; then
-    python3 -m venv venv
-fi
-
 source ./venv/bin/activate
-pip install --upgrade pip setuptools
-pip install .
-# full path to prevent killing any other bots with `bot.py` main script
-nohup python3 "${script_path}/bot.py" &
+pip3 install --upgrade pip setuptools
+pip3 install .
 
-popd
+sudo systemctl stop work_assistant_bot
+sudo systemctl stop work_assistant_bot
