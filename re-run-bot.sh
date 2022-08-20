a776e3a8817be3d7f112b7cb29ddfaf004838a10
atronah <atronah@users.noreply.github.com>
2022-08-20 21:03:48 +0300

    fixup!

--- re-run-bot.sh
+++ re-run-bot.sh
@@ -1,8 +1,18 @@
 #! /usr/bin/env bash
 
+script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+pushd "${script_path}"
+
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
 source ./venv/bin/activate
 pip3 install --upgrade pip setuptools
 pip3 install .
 
 sudo systemctl stop work_assistant_bot
 sudo systemctl start work_assistant_bot
+
+popd
