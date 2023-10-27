92d215800630e3149494e070797361533cff684f
atronah <atronah.ds@gmail.com>
2023-10-27 08:37:30 +0300

    ft: adds tryin to connect to otrs during signing in

--- time_tracker_bot/handlers.py
+++ time_tracker_bot/handlers.py
@@ -1,13 +1,18 @@
+import os
+
 from telegram import Update
 from telegram.ext import ContextTypes
+
+from .credentials import decrypt_credentials
+from .otrs_tools import get_otrs_client
+
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     u = update.effective_user
     await update.message.reply_text(f'Hello {u.username}, your ID is {u.id}')
-    
+
 
 async def die_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    import os
     u = update.effective_user
     owner_id = os.environ.get('TIME_TRACKER_BOT_OWNER_ID')
     if str(u.id) == owner_id:
@@ -23,14 +28,25 @@ async def signin_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_text = 'You are already signed in. Sign out first'
     elif len(context.args) == 2:
         name, key = context.args
-        credentials['name'] = name
-        credentials['key'] = key
-        reply_text = 'Credentials were saved'
+        
+        try: 
+            endpoint = os.environ.get('TIME_TRACKER_BOT_OTRS_ENDPOINT')
+            username, password = decrypt_credentials(name, key)
+            otrs_client = get_otrs_client(endpoint, username, password)
+
+            credentials['name'] = name
+            credentials['key'] = key
+            reply_text = 'Success. Credentials were saved'
+        except Exception as e:
+            reply_text = str(e)
     else:
         reply_text = '\n'.join(['Error: incorrect number of arguments',
                                 'Usage:', 
                                 '/signin NAME KEY'])
-    await update.message.reply_text(reply_text)
+    # remove message with private data in security reason
+    await update.message.delete()
+    await update.effective_chat.send_message(reply_text)
+
 
 async def signout_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     credentials = context.user_data.setdefault('credentials', dict())
---
+++ time_tracker_bot/otrs_tools.py
@@ -0,0 +1,13 @@
+def get_otrs_client(endpoint, username, password, webservice_name = 'GenericTicketConnectorSOAP'):
+    from otrs.ticket.template import GenericTicketConnectorSOAP
+    from otrs.client import GenericInterfaceClient
+
+    if not endpoint:
+        raise Exception('Empty endpoint for OTRS')
+    if not username or not password:
+        raise Exception('Empty credentials for OTRS')
+
+    otrs_client = GenericInterfaceClient(endpoint, tc=GenericTicketConnectorSOAP(webservice_name))
+    otrs_client.tc.SessionCreate(user_login=username, password=password)
+
+    return otrs_client
